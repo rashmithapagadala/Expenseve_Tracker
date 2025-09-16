2f56b01abf2cc49c8d68ee1c5fa1ed20ba18825a
rashmithapagadala <rashmithapagadala34@gmail.com>
2025-09-16 19:44:51 +0530

    Add files via upload

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCP671yIOeXC9nMim_Q6KekGzvh9e1XyNg",
+  authDomain: "expense-tracker-prod-9f00f.firebaseapp.com",
+  databaseURL: "https://expense-tracker-prod-9f00f-default-rtdb.firebaseio.com",
+  projectId: "expense-tracker-prod-9f00f",
+  storageBucket: "expense-tracker-prod-9f00f.firebasestorage.app",
+  messagingSenderId: "715668380968",
+  appId: "1:715668380968:web:4bb56b037fa3545a97c41d",
+  measurementId: "G-QJ2JZ82SLN"
+};
